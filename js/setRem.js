706df9cad48bf04cd9ad205372a6dea81c1329a7
刘茹 <2043524156@qq.com>
2022-11-16 19:48:42 +0800

    'L第一次上传'

--- js/setRem.js
+++ js/setRem.js
@@ -1,11 +1,9 @@
 function setRem() {
     var client_w = document.documentElement.clientWidth || document.body.clientWidth;
-    console.log(client_w);
-    client_w = client_w > 800 ? 800 : client_w;
+    client_w = client_w > 900 ? 900 : client_w;
     client_w = client_w < 350 ? 350 : client_w;
     var html = document.getElementsByTagName('html')[0];
     html.style.fontSize = (client_w / 375) * 10 + 'px';
-    console.log(client_w / 375 * 10 + 'px');
 }
 setRem();
 var time = null;
